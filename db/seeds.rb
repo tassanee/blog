2b201fe901147baf8cb68db79324be2088aed53d
kingkanbai <tassanee.oc@gmail.com>
2016-03-19 05:00:45 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+ Post.create!([
+     {title: 'My first blog (#1)',content: 'This is the content of my #1 blog'},
+     {title: 'My first blog (#2)',content: 'This is the content of my #2 blog'},
+     {title: 'My first blog (#3)',content: 'This is the content of my #3 blog'},
+     {title: 'My first blog (#4)',content: 'This is the content of my #4 blog'},
+     {title: 'My first blog (#5)',content: 'This is the content of my #5 blog'},
+    ])
